570d39ba35e3046f4343d69512a99d9ef7ee0a63
João Oliveira Lima Júnior <joao.jbsb@gmail.com>
2022-05-25 21:59:06 -0300

    inserindo a cor

--- contador/scripts.js
+++ contador/scripts.js
@@ -1,11 +1,13 @@
 
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
-console.log(currentNumber);
 
 function decrement(){
     currentNumber = currentNumber - 1
     currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumberWrapper.innerHTML < 0) {
+        currentNumberWrapper.style.color = 'red';
+    }
 }
 
 function increment(){
