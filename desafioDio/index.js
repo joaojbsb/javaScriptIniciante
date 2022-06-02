97b3ba3803a941cf7c24b14ac394274ac71e2142
João Oliveira Lima Júnior <joao.jbsb@gmail.com>
2022-06-01 21:36:23 -0300

    ok

---
+++ desafioDio/index.js
@@ -0,0 +1,16 @@
+
+ 
+
+ 
+let n = parseInt(14);
+let totalMatches = 0;
+    while(n>1){
+        if( n % 2 == 0){
+            for (let i = 0; i < n; i++) {
+                console.log(totalMatches = totalMatches + (n/2));                
+            }            
+        }else{
+            console.log("impar")
+        }
+    n = 0;
+    }
